c61612b29ee4f059bacdcd6ba0e52b62cbf5f938
ohk990102 <ohk990102@gmail.com>
2023-11-08 21:31:08 +0900

    [6] add modular binary inversion

---
+++ 6/src/ecc.h
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define _GMP_ENABLE_
+
+#ifdef _GMP_ENABLE_
+#include "gmp.h"
+#endif
+
+#define ECC_PASS 1
+#define ECC_FAIL -1
+#define ECC_WORD_LEN 20
+
+typedef struct _ECC_BN {
+  unsigned int dat[ECC_WORD_LEN];
+  unsigned int len;
+} ECC_BN;
+
+// FFFFFFFF 00000001 00000000 00000000 00000000 FFFFFFFF FFFFFFFF FFFFFFFF
+static ECC_BN prime_p256 = {{0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0x00000000,
+                             0x00000000, 0x00000000, 0x00000001, 0xFFFFFFFF},
+                            8};
+
+#ifdef _GMP_ENABLE_
+int mpz_to_ECC_BN(ECC_BN* c, mpz_t a);
+int ECC_BN_to_mpz(mpz_t c, ECC_BN* a);
+#endif
+
+int ECC_BN_copy(ECC_BN* c, ECC_BN* a);  // c = a
+int ECC_BN_cmp(ECC_BN* a, ECC_BN* b);   // a>b => 1 , a==b => 0, a<b => -1
+int ECC_BN_lshift_1bit(ECC_BN* c, ECC_BN* a);  // c= a<<1
+
+void __ECC_BN_add(ECC_BN* c, ECC_BN* a, ECC_BN* b);  // c = a+b
+void __ECC_BN_sub(ECC_BN* c, ECC_BN* a, ECC_BN* b);  // c = a-b
+
+int ECC_BN_add_mod(ECC_BN* c, ECC_BN* a, ECC_BN* b, ECC_BN* p);  // c= a+b mod p
+int ECC_BN_sub_mod(ECC_BN* c, ECC_BN* a, ECC_BN* b, ECC_BN* p);  // c= a-b mod p
+
+void __ECC_BN_mul(ECC_BN* c, ECC_BN* a, ECC_BN* b);        // c = a+b
+int ECC_BN_mod_p256(ECC_BN* c, ECC_BN* a);                 // c = a mod p256
+int ECC_BN_mul_mod_p256(ECC_BN* c, ECC_BN* a, ECC_BN* b);  // c = a+b mod p256
+int ECC_BN_mul_mod(ECC_BN* c, ECC_BN* a, ECC_BN* b,
+                   ECC_BN* p);  // c = a+b mod p
+
+int ECC_BN_rshift_1bit(ECC_BN* c, ECC_BN* a); // c= a>>1
+int ECC_BN_binary_inv(ECC_BN* c, ECC_BN* a); // c= a^(-1) mod p 
---
+++ 6/src/test.c
@@ -0,0 +1,290 @@
+#include "ecc.h"
+
+void test___ecc_bn_add() {
+  int i, len;
+  mpz_t a, b, c;
+  ECC_BN ec_a, ec_b, ec_c, tmp;
+  gmp_randstate_t state;
+
+  mpz_init(a);
+  mpz_init(b);
+  mpz_init(c);
+  gmp_randinit_default(state);
+
+  for (i = 0; i < 100000; i++) {
+    mpz_urandomb(a, state, 256);
+    mpz_urandomm(b, state, a);
+
+    len = a->_mp_d[0] % 10;
+    b->_mp_size = (len) ? (b->_mp_size % len) : (b->_mp_size);
+
+    mpz_add(c, a, b);
+
+    mpz_to_ECC_BN(&ec_a, a);
+    mpz_to_ECC_BN(&ec_b, b);
+    __ECC_BN_add(&ec_c, &ec_a, &ec_b);
+
+    mpz_to_ECC_BN(&tmp, c);
+
+    if (ECC_BN_cmp(&ec_c, &tmp)) {
+      printf("\ntest ECC_BN_add FAIL\n");
+      return;
+    }
+
+    gmp_printf("a=%Zx\n", a);
+    gmp_printf("b=%Zx\n", b);
+    gmp_printf("c=%Zx\n", c);
+  }
+
+  printf("\ntest ECC_BN_add PASS\n");
+
+  mpz_clear(a);
+  mpz_clear(b);
+  mpz_clear(c);
+  gmp_randclear(state);
+}
+
+void test___ecc_bn_sub() {
+  int i, len;
+  mpz_t a, b, c;
+  ECC_BN ec_a, ec_b, ec_c, tmp;
+  gmp_randstate_t state;
+
+  mpz_init(a);
+  mpz_init(b);
+  mpz_init(c);
+  gmp_randinit_default(state);
+
+  for (i = 0; i < 100000; i++) {
+    mpz_urandomb(a, state, 256);
+    mpz_urandomm(b, state, a);
+
+    len = a->_mp_d[0] % 10;
+    b->_mp_size = (len) ? (b->_mp_size % len) : (b->_mp_size);
+
+    mpz_sub(c, a, b);
+
+    mpz_to_ECC_BN(&ec_a, a);
+    mpz_to_ECC_BN(&ec_b, b);
+    __ECC_BN_sub(&ec_c, &ec_a, &ec_b);
+
+    mpz_to_ECC_BN(&tmp, c);
+
+    if (ECC_BN_cmp(&ec_c, &tmp)) {
+      printf("\ntest ECC_BN_sub FAIL\n");
+      return;
+    }
+
+    gmp_printf("a=%Zx\n", a);
+    gmp_printf("b=%Zx\n", b);
+    gmp_printf("c=%Zx\n", c);
+  }
+
+  printf("\ntest ECC_BN_sub PASS\n");
+
+  mpz_clear(a);
+  mpz_clear(b);
+  mpz_clear(c);
+  gmp_randclear(state);
+}
+
+void test___ecc_bn_add_mod() {
+  int i;
+  mpz_t a, b, c, d;
+  ECC_BN ec_a, ec_b, ec_c, tmp;
+  gmp_randstate_t state;
+
+  mpz_init(a);
+  mpz_init(b);
+  mpz_init(c);
+  mpz_init(d);
+
+  ECC_BN_to_mpz(d, &prime_p256);
+  gmp_randinit_default(state);
+
+  for (i = 0; i < 100000; i++) {
+    // mpz_urandomb(a, state, 256);
+    mpz_urandomm(a, state, d);
+    mpz_urandomm(b, state, d);
+
+    // len = a->_mp_d[0] % 10;
+    // b->_mp_size = (len) ? (b->_mp_size%len) : (b->_mp_size);
+
+    mpz_add(c, a, b);
+    mpz_mod(c, c, d);
+
+    mpz_to_ECC_BN(&ec_a, a);
+    mpz_to_ECC_BN(&ec_b, b);
+    ECC_BN_add_mod(&ec_c, &ec_a, &ec_b, &prime_p256);
+
+    mpz_to_ECC_BN(&tmp, c);
+
+    if (ECC_BN_cmp(&ec_c, &tmp)) {
+      printf("\ntest ECC_BN_add_mod FAIL\n");
+      return;
+    }
+
+    gmp_printf("a=%Zx\n", a);
+    gmp_printf("b=%Zx\n", b);
+    gmp_printf("c=%Zx\n", c);
+  }
+
+  printf("\ntest ECC_BN_add_mod PASS\n");
+
+  mpz_clear(a);
+  mpz_clear(b);
+  mpz_clear(c);
+  gmp_randclear(state);
+}
+
+void test___ecc_bn_sub_mod() {
+  int i;
+  mpz_t a, b, c, d;
+  ECC_BN ec_a, ec_b, ec_c, tmp;
+  gmp_randstate_t state;
+
+  mpz_init(a);
+  mpz_init(b);
+  mpz_init(c);
+  mpz_init(d);
+
+  ECC_BN_to_mpz(d, &prime_p256);
+  gmp_randinit_default(state);
+
+  for (i = 0; i < 100000; i++) {
+    // mpz_urandomb(a, state, 256);
+    mpz_urandomm(a, state, d);
+    mpz_urandomm(b, state, d);
+
+    // len = a->_mp_d[0] % 10;
+    // b->_mp_size = (len) ? (b->_mp_size%len) : (b->_mp_size);
+
+    mpz_sub(c, a, b);
+    mpz_mod(c, c, d);
+
+    mpz_to_ECC_BN(&ec_a, a);
+    mpz_to_ECC_BN(&ec_b, b);
+    ECC_BN_sub_mod(&ec_c, &ec_a, &ec_b, &prime_p256);
+
+    mpz_to_ECC_BN(&tmp, c);
+
+    if (ECC_BN_cmp(&ec_c, &tmp)) {
+      printf("\ntest ECC_BN_sub_mod FAIL\n");
+      return;
+    }
+
+    gmp_printf("a=%Zx\n", a);
+    gmp_printf("b=%Zx\n", b);
+    gmp_printf("c=%Zx\n", c);
+  }
+
+  printf("\ntest ECC_BN_sub_mod PASS\n");
+
+  mpz_clear(a);
+  mpz_clear(b);
+  mpz_clear(c);
+  gmp_randclear(state);
+}
+
+void test___ecc_bn_mul_mod_p256() {
+  int i;
+  mpz_t a, b, c, d;
+  ECC_BN ec_a, ec_b, ec_c, tmp;
+  gmp_randstate_t state;
+
+  mpz_init(a);
+  mpz_init(b);
+  mpz_init(c);
+  mpz_init(d);
+
+  ECC_BN_to_mpz(d, &prime_p256);
+  gmp_randinit_default(state);
+
+  for (i = 0; i < 100000; i++) {
+    mpz_urandomm(a, state, d);
+    mpz_urandomm(b, state, d);
+
+    a->_mp_size = 2;
+    b->_mp_size = 2;
+
+    mpz_mul(c, a, b);
+    mpz_mod(c, c, d);
+
+    mpz_to_ECC_BN(&ec_a, a);
+    mpz_to_ECC_BN(&ec_b, b);
+    ECC_BN_mul_mod_p256(&ec_c, &ec_a, &ec_b);
+
+    mpz_to_ECC_BN(&tmp, c);
+
+    gmp_printf("a=%Zx\n", a);
+    gmp_printf("b=%Zx\n", b);
+    gmp_printf("c=%Zx\n", c);
+
+    if (ECC_BN_cmp(&ec_c, &tmp)) {
+      printf("\ntest ECC_BN_mul_mod_p256 FAIL\n");
+      ECC_BN_to_mpz(d, &ec_c);
+      gmp_printf("ec_c=%Zx\n", d);
+      return;
+    }
+  }
+
+  printf("\ntest ECC_BN_mul_mod_p256 PASS\n");
+
+  mpz_clear(a);
+  mpz_clear(b);
+  mpz_clear(c);
+  gmp_randclear(state);
+}
+
+void test___ecc_bn_binary_inv() {
+  int i;
+  mpz_t a, p, a_inv;
+  gmp_randstate_t state;
+
+  ECC_BN ec_a, ec_expect, ec_got;
+
+  mpz_init(a);
+  mpz_init(p);
+  mpz_init(a_inv);
+  gmp_randinit_default(state);
+
+  ECC_BN_to_mpz(p, &prime_p256);
+
+  for (i = 0; i < 100000; i++) {
+    mpz_urandomm(a, state, p);
+    // mpz_set_ui(a, 3);
+    mpz_invert(a_inv, a, p);
+
+    mpz_to_ECC_BN(&ec_a, a);
+    mpz_to_ECC_BN(&ec_expect, a_inv);
+
+    ECC_BN_binary_inv(&ec_got, &ec_a);
+
+    gmp_printf("a=%Zx\n", a);
+    gmp_printf("p=%Zx\n", p);
+    gmp_printf("a_inv_expect=%Zx\n", a_inv);
+
+    if (ECC_BN_cmp(&ec_got, &ec_expect)) {
+      printf("\ntest ECC_BN_binary_inv FAIL\n");
+      ECC_BN_to_mpz(p, &ec_got);
+      gmp_printf("a_inv_got=%Zx\n", p);
+      return;
+    }
+  }
+
+  printf("\ntest ECC_BN_binary_inv PASS\n");
+
+  mpz_clear(a);
+  mpz_clear(p);
+  mpz_clear(a_inv);
+  gmp_randclear(state);
+}
+
+int main() {
+  // test___ecc_bn_add();
+  // test___ecc_bn_sub();
+  //  test___ecc_bn_add_mod();
+  //  test___ecc_bn_sub_mod();
+  // test___ecc_bn_mul_mod_p256();
+  test___ecc_bn_binary_inv();
+}
